36d0cc569916b42492a1dc558b899278acb69507
tu-usuario <tu-email>
2025-04-28 21:55:14 -0700

    FrmMenuPartida
    -Sin terminar

---
+++ Presentacion/src/main/java/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import presentacion.FrmInicio;
+
+/**
+ *
+ * @author Oley
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        FrmInicio  frmInicio=new FrmInicio();
+        frmInicio.setVisible(true);
+        
+    }
+}
